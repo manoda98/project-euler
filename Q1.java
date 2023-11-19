321df7e761e26d096df71f800462c317549130df
manoda98 <2020e147@eng.jfn.ac.lk>
2023-11-19 20:39:27 +0530

    problem 01 is done

--- Q1.java
+++ Q1.java
@@ -1,5 +1,13 @@
 public class Q1 {
     public static void main(String [] args) {
-        System.out.println("Hello World");
+        int n;
+        n = 1000;
+        int sum = 0;
+        for( int i = 1 ; i < n ; i ++)
+        if((i % 3 == 0) || (i % 5 == 0)) {
+            sum = sum + i;
+        }
+            System.out.println("Answer is : " + sum);
+        }
     }
-}
+

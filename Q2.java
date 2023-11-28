61e4c57a6c48c14c458c62e9d3686340032ccb62
manoda98 <2020e147@eng.jfn.ac.lk>
2023-11-28 08:32:03 +0530

    sum of even numbers of a fibonacci sequence

--- Q2.java
+++ Q2.java
@@ -1,18 +1,18 @@
 public class Q2 {
     public static void main(String [] args) {
-        int x = 1;
-        int y = 2;
+        int a = 1;
+        int b = 2;
         int sum = 2;
         int limit = 4000000;
         int nextTerm = 0;
 
         while(nextTerm <= limit) {
-            nextTerm = x + y;
+            nextTerm = a + b;
             if(nextTerm % 2 == 0 && nextTerm <= limit) {
                 sum = sum + nextTerm;
             }
-            x = y;
-            y = nextTerm;
+            a = b;
+            b = nextTerm;
         }
         System.out.println("sum of even value : " + sum);
     }
